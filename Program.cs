77f6ca1ef16cad90a9e39b41870cfb08116f3f3f
Hemant <hemant.shelar@gmail.com>
2025-02-01 19:02:40 +1100

    setup user name and password in secrets.json.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@
 //Top level statements
 
 using Asha.Data;
+using Asha.Domain;
 using dotnet_core_pi3bplus.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,6 +17,13 @@ builder.Services.AddScoped<ICar, Car>();
 builder.Services.AddScoped<IToyotaCar, ToyotaCar>();
 builder.Services.AddScoped<IPalisadeCar, PalisadeCar>();
 builder.Services.AddTransient<Customer>();
+
+builder.Configuration.AddUserSecrets<Program>();
+
+#region Configuration Settings for SQL Server
+builder.Services.Configure<SqlSettings>(builder.Configuration.GetSection("SqlSettings"));
+builder.Services.AddScoped<AshaContext>();
+#endregion
 
 
 IHost host = builder.Build();
@@ -33,15 +43,46 @@ static void Entry(IServiceProvider serviceProvider)
     Customer anotherCust = serviceProvider.GetRequiredService<Customer>();
     anotherCust.IsDriving();
 
-    TestDb();
+    TestDb(serviceProvider);
 }
 
 
-static void TestDb()
+static void TestDb(IServiceProvider serviceProvider)
 {
-    using (var db = new AshaContext())
+    try
     {
-        db.Database.EnsureCreated();
-        db.SaveChanges();
+        var context = serviceProvider.GetRequiredService<AshaContext>();
+        context.Database.EnsureCreated();
+        //Read books
+
+        var books = context.Books.Include(b => b.Author).ToList();
+        if (books.Count == 0)
+        {
+
+            books = new List<Book>
+        {
+        new Book { Title = "Book1", PublishDate = DateOnly.FromDateTime(DateTime.Now),BasePrice = 10},
+        new Book { Title = "Book2",PublishDate = DateOnly.FromDateTime(DateTime.Now), BasePrice= 20},
+        new Book { Title = "Book3",PublishDate = DateOnly.FromDateTime(DateTime.Now), BasePrice= 30}
+        };
+
+            var author = new Author { FirstName = "Hemant", LastName = "Shelar", Books = books };
+            context.Authors.Add(author);
+            context.SaveChanges();
+        }
+        else
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.Title} - {book.Author.FirstName} {book.Author.LastName}");
+            }
+
+        }
+
+
+    }
+    catch (Exception ex)
+    {
+        throw;
     }
 }
